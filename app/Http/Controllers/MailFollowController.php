5270409639ae2b78c4dd1a43c25122654b65a23e
dr.elhabib <work.elhabib@gmail.com>
2023-03-28 10:30:38 +0200

    sort

--- app/Http/Controllers/MailFollowController.php
+++ app/Http/Controllers/MailFollowController.php
@@ -12,7 +12,7 @@ class MailFollowController extends Controller
 
     public function list()
     {
-        $mailFollows = MailFollow::all()->sortBy("date","asc");
+        $mailFollows = MailFollow::all()->sortBy("date" ,[], false);
         return view("mail-follow.list", ["mailFollows" => $mailFollows]);
     }
     public function add()
